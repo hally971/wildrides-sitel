6a0583c1f0b0796c843f61a196afa8a667b73514
hally971 <hally971@gmail.com>
2025-06-08 15:58:41 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_modpznXrI', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7dc5q2tra9vs8t0s5v9gk4pp5q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
